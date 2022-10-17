c029c35c01eb2e506760d794809d99dd4dc93040
ved1150 <vppatel1771@gmail.com>
2022-10-17 17:33:08 +0530

    build signup form with database

---
+++ src/--AUTHENTICATION--/AuthForm.js
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from "react";
+import "./AuthForm.css";
+export default function AuthForm() {
+  const [isLogin, setIsLogin] = useState(true);
+  const email = useRef();
+  const password = useRef();
+  const conPassword = useRef();
+  function changeLoginState() {
+    setIsLogin((pre) => !pre);
+  }
+  function formHandler(event) {
+    event.preventDefault();
+
+    let emailEntered = email.current.value;
+    let passwordEntered = password.current.value;
+    let conPasswordEntered = conPassword.current.value;
+    if (passwordEntered !== conPasswordEntered) {
+      alert("Password not match");
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC5BXr8sSDXdJ4Ye8lN8J9vnNi0s3nXtVg",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: emailEntered,
+            password: passwordEntered,
+            returnSecureToken: true,
+          }),
+        }
+      ).then((res) => {
+        if (res.ok) {
+          console.log("User has successfully signed up.");
+          alert("Sign-up successfully ");
+        }else{
+          res.json().then(data => alert(data.error.message))
+        }
+      });
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={formHandler}>
+        <div className="authForm">
+          <h1> {isLogin ? "Login" : "Sign-up"}</h1>
+          <input type="text" placeholder="Email" required ref={email} />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            ref={password}
+          />
+          {!isLogin && (
+            <input
+              type="password"
+              placeholder="confirm Password"
+              required
+              ref={conPassword}
+            />
+          )}
+          <button>{isLogin ? "Login" : "Sign-up"}</button>
+          <button onClick={changeLoginState}>
+            {isLogin ? "what to creat a new account" : "have a account ? login"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Route } from 'react-router-dom'
+import AuthForm from './--AUTHENTICATION--/AuthForm'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Route path="./"> */}
+        <AuthForm />
+      {/* </Route> */}
     </div>
-  );
+  )
 }
-
-export default App;
